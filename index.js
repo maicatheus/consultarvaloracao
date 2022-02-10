3380fee8a68a2474cff5f0922c09158937cd2dd5
maicatheus <maicatheus@gmail.com>
2022-02-10 16:05:08 -0300

    include Procfile

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const session = require('express-session')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000
 var path = require('path')
 const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    app.use(cors())
+    next()
+})
 
 
 app.use(session({secret:'asuiod093ondfg'}))
